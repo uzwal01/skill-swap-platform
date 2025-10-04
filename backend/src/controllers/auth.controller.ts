1adc388a14f20ddbf1cee5062adbd87582bee473
uzwal01 <uzwalduwal07@gmail.com>
2025-10-04 13:20:30 +0545

    feat: implement user registration and login with JWT and also fixed test errors

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,82 @@
+import { Request, Response } from "express";
+import { User } from "../models/User";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// Register User
+export const registerUser = async (req: Request, res: Response) => {
+    try {
+        const { name, email, password, skillsOffered, skillsWanted } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            skillsOffered,
+            skillsWanted,
+        });
+
+        const JWT_SECRET = process.env.JWT_SECRET as string;
+
+        const token = jwt.sign({ userId: newUser._id }, JWT_SECRET, { expiresIn: '7d' });
+
+        res.status(201).json({
+            user: {
+                id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                skillsOffered: newUser.skillsOffered,
+                skillsWanted: newUser.skillsWanted,
+            },
+            token,
+        });
+    } catch (err) {
+        console.error('Register Error:', err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+
+
+// Login User
+export const loginUser = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(400).json({ message: 'Invalid email or password'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const JWT_SECRET = process.env.JWT_SECRET as string;
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '7d' });
+
+        res.json({
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                skillsOffered: user.skillsOffered,
+                skillsWanted: user.skillsWanted,
+            },
+            token,
+        });
+
+    } catch (err) {
+        console.error('Login Error:', err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
-
+import authRoutes from './routes/v1/auth.routes';
 
 dotenv.config();
 
@@ -14,11 +14,16 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+
+
 // Route
 app.get('/api/v1/health', (req, res) => {
     res.status(200).json({ status: 'OK', message: 'SkillSwap API is working' });
 });
 
+app.use('/api/v1/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
